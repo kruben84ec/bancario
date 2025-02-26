d9b2ff860f10a1a2e16b6a7d4702fef7c4200a7a
kruben84ec <crmirandazambrano@gmail.com>
2025-02-26 03:39:09 -0500

    Creacion del proyecto de spring boot

--- bancario/src/main/java/com/banco/cliente_service/repository/MovimientosRepository.java
+++
@@ -1,7 +0,0 @@
-package com.banco.cliente_service.repository;
-
-import com.banco.cliente_service.entity.Movimientos;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovimientosRepository extends JpaRepository<Movimientos, Long> {
-}
--- bancario/src/main/java/com/banco/cliente_service/service/MovimientosService.java
+++
@@ -1,7 +0,0 @@
-package com.banco.cliente_service.service;
-
-import com.banco.cliente_service.dto.MovimientosDTO;
-
-public interface MovimientosService {
-    MovimientosDTO registrarMovimiento(MovimientosDTO movimientosDTO);
-}
